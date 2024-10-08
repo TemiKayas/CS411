449468e48b728cfd8df94c9302db76cc6910c94e
Temi Kayas <99490404+TemiKayas@users.noreply.github.com>
2024-10-08 11:30:07 -0400

    All of the python files for parts 1 and 2

---
+++ wildlife_tracker/migration_tracking/migration.py
@@ -0,0 +1,20 @@
+from typing import Any, Optional
+
+from .migration_path import MigrationPath
+
+class Migration:
+    def __init__(self,
+                status: str = "Scheduled",
+                migration_path: MigrationPath = None,
+                migration_id = int,  
+                current_location = str) -> None:
+        self.current_location = current_location
+        self.status = status
+        self.migration_path = migration_path
+        self.migration_id = migration_id
+
+def get_migration_details(self) -> dict[str, Any]:
+    pass
+
+def update_migration_details(self, **kwargs: Any) -> None:
+    pass
